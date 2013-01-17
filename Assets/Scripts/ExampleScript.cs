f594b7cbaf36d78517f86e2c543ae437df0cdae9
Michael Carter <mcarter@simutronics.com>
2013-01-16 20:32:23 -0600

    example script updated

--- Assets/Scripts/ExampleScript.cs
+++ Assets/Scripts/ExampleScript.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class ExampleScript : MonoBehaviour {
-
+	
+	Rigidbody body;
+	public float bounceInterval = 5f;
+	float nextBounce;
+	
 	// Use this for initialization
 	void Start () {
-	
+		body = rigidbody;
+		nextBounce = bounceInterval;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Time.time > nextBounce)
+		{
+			nextBounce += bounceInterval;
+			body.AddForce(Vector3.up*500);
+		}
 	}
 }
