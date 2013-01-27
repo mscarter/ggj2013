05aa06b9a0f7eea479d293e3ddd84ab86f7e62ec
Michael Carter <mcarter@simutronics.com>
2013-01-26 18:10:08 -0600

    Editor Script to create a character heirarchy.

--- Assets/Editor/CreateDialogEditor.cs
+++ Assets/Editor/CreateDialogEditor.cs
@@ -4,16 +4,39 @@ using System.Collections;
 
 public class CreateDialogEditor : ScriptableObject {
 	
-	[MenuItem ("GameObject/Create Other/Character Dialog")]
-	public static void MenuCreateCharacterDialog()
+	[MenuItem ("GameObject/Create Other/Character Tree")]
+	public static void MenuCreateCharacterTree()
 	{
-        Transform[] transforms = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+//        Transform[] transforms = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
  
-        foreach(Transform transform in transforms)
-        {
-            GameObject newChild = new GameObject("Character Dialog", typeof(CharacterDialog));
-            newChild.transform.parent = transform;
-			newChild.transform.localPosition = Vector3.zero;
-        }
+		GameObject characterObject = new GameObject("Character Name", typeof(ClubPatron));
+		var patron = characterObject.GetComponent<ClubPatron>();
+		
+		patron.initialQuestion1 = CreateDialog(1, characterObject.transform, null);
+		patron.initialQuestion2 = CreateDialog(1, characterObject.transform, null);
+		patron.initialQuestion3 = CreateDialog(1, characterObject.transform, null);
+	}
+	
+	static CharacterDialog CreateDialog(int depth, Transform parent, CharacterDialog firstDialog)
+	{
+		GameObject dialogObject = new GameObject("Character Dialog", typeof(CharacterDialog));
+		dialogObject.transform.parent = parent;
+		
+		var dialog = dialogObject.GetComponent<CharacterDialog>();
+		
+		if (depth == 1)
+		{
+			firstDialog = dialog;
+		}
+		
+		if (depth < 3)
+		{
+			dialog.nextDialog = CreateDialog(depth + 1, dialogObject.transform, firstDialog);
+		}
+		else
+		{
+			dialog.nextDialog = firstDialog;
+		}
+		return dialog;
 	}
 }
